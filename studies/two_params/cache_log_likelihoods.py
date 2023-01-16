375d5cf83fb4d3f69b4d4d09ad1df829824c759a
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-01-16 15:17:49 +1100

    allow scatter plot

--- studies/two_params/cache_log_likelihoods.py
+++ studies/two_params/cache_log_likelihoods.py
@@ -15,20 +15,27 @@ GLOB_STR = f"{OUTDIR}/*.npz"
 CACHE_LNL_FILE = "cache_lnl.npz"
 
 
-def plot_2d_density(x, y, z, true_x=None, true_y=None):
+def plot_2d_density(x, y, z, true_x=None, true_y=None, scatter=False):
     """
     Plot a 2D density plot
     """
     fig, ax = plt.subplots()
-    cmap = ax.tricontourf(x, y, z, 100, cmap="viridis")
+    if scatter:
+        cmap = ax.scatter(x, y, c=z, cmap="viridis", s=1)
+    else:
+        cmap = ax.tricontourf(x, y, z, 100, cmap="viridis")
+
     if true_x is not None and true_y is not None:
         ax.scatter(true_x, true_y, marker="x", color="red", s=100)
-        ax.axvline(true_x, color="red")
-        ax.axhline(true_y, color="red")
+        ax.axvline(true_x, color="red", alpha=0.1)
+        ax.axhline(true_y, color="red", alpha=0.1)
+
+    ax.set_aspect("equal")
     ax.set_xlabel("muz")
     ax.set_ylabel("sigma0")
     fig.colorbar(cmap, label="LnL")
-    fig.savefig("lnl_2d_density.png")
+    fig.tight_layout()
+    fig.savefig("lnl_2d_density" + "_scatter" * scatter + ".png")
 
 
 def main(universes_glob=GLOB_STR, cache_lnl_file=CACHE_LNL_FILE):
@@ -36,16 +43,19 @@ def main(universes_glob=GLOB_STR, cache_lnl_file=CACHE_LNL_FILE):
     random_uni = random.choice(universe_paths)
     observed_uni = Universe.from_npz(random_uni)
     mock_population = observed_uni.sample_possible_event_matrix()
+    mock_population.plot(save=True)
     compute_and_cache_lnl(mock_population, universe_paths, cache_lnl_file)
 
     data_dict = load_lnl_cache("cache_lnl.npz")
-    plot_2d_density(
-        data_dict["muz"],
-        data_dict["sigma0"],
-        data_dict["lnl"],
+    plt_kwgs = dict(
+        x=data_dict["muz"],
+        y=data_dict["sigma0"],
+        z=data_dict["lnl"],
         true_x=data_dict["true_params"]["muz"],
         true_y=data_dict["true_params"]["sigma0"],
     )
+    plot_2d_density(**plt_kwgs)
+    plot_2d_density(**plt_kwgs, scatter=True)
 
 
 if __name__ == "__main__":
