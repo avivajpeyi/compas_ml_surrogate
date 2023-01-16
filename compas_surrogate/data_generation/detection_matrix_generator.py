6d512820f12c0b25d75bfc6857f950203248ab37
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-01-17 10:30:40 +1100

    add chunk size

--- compas_surrogate/data_generation/detection_matrix_generator.py
+++ compas_surrogate/data_generation/detection_matrix_generator.py
@@ -72,7 +72,9 @@ def generate_set_of_matricies(
     )
 
     args = ([compas_h5_path] * n, sf_samples, [save_images] * n, [outdir] * n)
-    process_map(generate_matrix, *args, max_workers=get_num_workers())
+    process_map(
+        generate_matrix, *args, max_workers=get_num_workers(), chunksize=100
+    )
 
     if save_images:
         logger.info("Making GIFs")
