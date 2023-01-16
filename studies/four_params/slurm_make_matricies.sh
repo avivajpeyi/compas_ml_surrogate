1e34f220907378c5e3a5402da98e4ed1b30a8245
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-01-17 10:17:43 +1100

    add matrix gen

---
+++ studies/four_params/slurm_make_matricies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=make_matricies
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=6
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --output=log_det_matrix_%j.out
+#SBATCH --dependency=singleton
+
+module gcc/9.2.0 openmpi/4.0.2 numpy/1.19.2-python-3.8.5 mpi4py/3.0.3-python-3.8.5 && module unload zlib && module load pandas/1.2.2-python-3.8.5
+source /fred/oz980/avajpeyi/envs/compas_venv/bin/activate
+
+python make_detection_matricies.py
