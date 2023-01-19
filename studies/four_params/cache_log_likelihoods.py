22f7a8229fee1bbec212e9d80770854a84a7754e
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-01-19 17:13:43 +1100

    testing surrogate

--- studies/four_params/cache_log_likelihoods.py
+++ studies/four_params/cache_log_likelihoods.py
@@ -10,23 +10,38 @@ from compas_surrogate.data_generation.likelihood_cacher import (
     LikelihoodCache,
     compute_and_cache_lnl,
 )
+from compas_surrogate.logger import logger
+from compas_surrogate.surrogate.models import DeepGPModel
 
-OUTDIR = "out_test_lnl"
+OUTDIR = "out_surr"
 CACHE_LNL_FILE = f"{OUTDIR}/cache_lnl.npz"
 H5 = "det_matrix.h5"
 
 
-def main(cache_lnl_file=CACHE_LNL_FILE):
+def generate_cache(cache_lnl_file=CACHE_LNL_FILE) -> LikelihoodCache:
+    if os.path.exists(cache_lnl_file):
+        logger.info(f"Loading cache from {cache_lnl_file}")
+        return LikelihoodCache.from_npz(cache_lnl_file)
+    else:
+        logger.info(f"Generating cache {cache_lnl_file}")
+        h5_file = h5py.File(H5, "r")
+        observed_uni = Universe.from_hdf5(h5_file, random.randint(0, 1000))
+        mock_population = observed_uni.sample_possible_event_matrix()
+        mock_population.plot(save=True, outdir=OUTDIR)
+        lnl_cache = compute_and_cache_lnl(
+            mock_population, cache_lnl_file, h5_path=H5
+        )
+        lnl_cache.plot(fname=f"{OUTDIR}/lnl_cache_{mock_population}.png")
+    return lnl_cache
+
+
+def main():
     os.makedirs(OUTDIR, exist_ok=True)
-    h5_file = h5py.File(H5, "r")
-    observed_uni = Universe.from_hdf5(h5_file, random.randint(0, 1000))
-    mock_population = observed_uni.sample_possible_event_matrix()
-    mock_population.plot(save=True, outdir=OUTDIR)
-    lnl_cache = compute_and_cache_lnl(
-        mock_population, cache_lnl_file, h5_path=H5
-    )
-    lnl_cache = LikelihoodCache.from_npz(cache_lnl_file)
-    lnl_cache.plot(fname=f"{OUTDIR}/lnl_cache_{mock_population}.png")
+    cache = generate_cache()
+    model = DeepGPModel()
+    model.train(cache.params, cache.lnl.reshape(-1, 1))
+    model.save(OUTDIR)
+    logger.success("Done")
 
 
 if __name__ == "__main__":
