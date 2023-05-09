fe473deaa47c2303b44547407152ebe8359e677d
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-05-09 19:17:29 +1200

    add module loads

--- studies/uncertainty_testing/slurm_generate_matrices_narrow.sh
+++ studies/uncertainty_testing/slurm_generate_matrices_narrow.sh
@@ -7,9 +7,7 @@
 #SBATCH --mem-per-cpu=22G
 #SBATCH --account=oz101
 
-module load git/2.18.0 git-lfs/2.4.0 gcc/9.2.0 openmpi/4.0.2 numpy/1.19.2-python-3.8.5 mpi4py/3.0.3-python-3.8.5
-module unload zlib
-module load pandas/1.2.2-python-3.8.5
-source /fred/oz980/avajpeyi/envs/compas_venv/bin/activate
+module load python-scientific/3.10.4-foss-2022a
+source /fred/oz980/avajpeyi/envs/milan_venv/bin/activate
 
 python make_det_matricies.py --compas_file_regex "sampled_datasets/*.h5" --outdir sampled_datasets/muz_sigam_narrow_matricies -n 1500
