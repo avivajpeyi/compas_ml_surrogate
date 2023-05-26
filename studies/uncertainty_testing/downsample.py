b6f41bb631b523ca405107f71c3ab17c662a8605
avi <avi.vajpeyi@gmail.com>
2023-05-26 14:02:59 +1200

    add array job

--- studies/uncertainty_testing/downsample.py
+++
@@ -1,6 +0,0 @@
-"""
-This script makes 10 upsampled versions of the COMPAS h5 file, each with N+sqrt(N) DCO binaries and different sampling seeds.
-"""
-from compas_surrogate.preprocessing.sample_compas_dcos import downsample
-
-downsample("./DCO_data.h5", outdir="downsampled_datasets", n_copies=10)
