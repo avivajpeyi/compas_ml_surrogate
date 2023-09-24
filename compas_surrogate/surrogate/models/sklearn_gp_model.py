model prediction scaling error fix?

compas_surrogate/surrogate/models/sklearn_gp_model.py
@@ -16,11 +16,8 @@ MODEL_SAVE_FILE = "model.pkl"
 
 class SklearnGPModel(Model):
     def __init__(self):
-        self._model = None
-        self.trained = False
-        self.input_dim = None
+        super().__init__()
         self.kernel = 1.0 * RBF(length_scale=1.0, length_scale_bounds=(1e-2, 1e3))
-        self.scalar = None
 
         # # diff between every pair of train_out values
         # err = np.min(np.diff(train_out, axis=0) ** 2)
@@ -66,7 +63,6 @@ class SklearnGPModel(Model):
             test_out,
         ) = self._preprocess_and_split_data(inputs, outputs)
 
-        # diff between every pair of train_out values
         err = halfnorm.rvs(loc=0, scale=0.5, size=len(train_in))
         self._model = GaussianProcessRegressor(
             kernel=self.kernel,
@@ -84,7 +80,8 @@ class SklearnGPModel(Model):
 
     def predict(self, x: np.ndarray) -> np.ndarray:
         """Predict the output of the model for the given input."""
-        x_scaled = self._preprocess_input(x)
+        # x_scaled = self._preprocess_input(x)
+        x_scaled = x  # TODO: bug with scaling -- i might be scaling twice
         y_mean, y_std = self._model.predict(x_scaled, return_std=True)
         y_var = y_std**2
         y_lower = y_mean - 1.96 * np.sqrt(y_var)
@@ -121,14 +118,3 @@ class SklearnGPModel(Model):
 
     def get_model(self):
         return self._model
-
-
-# Custom callback to save R^2 scores during training
-class R2Callback(BaseEstimator):
-    def __init__(self):
-        self.r2_scores = []
-
-    def on_epoch_end(self, epoch, logs=None):
-        y_pred = self.model.predict(self.validation_data[0])
-        r2 = r2_score(self.validation_data[1], y_pred)
-        self.r2_scores.append(r2)

tests/test_surrogate_models.py
@@ -11,7 +11,7 @@ np.random.seed(1)
 
 TEAR_DOWN = False
 
-from compas_surrogate.surrogate.models import DeepGPModel, SklearnGPModel
+from compas_surrogate.surrogate.models import SklearnGPModel
 
 CURVY_F = (
     lambda x: 0.2 + 0.4 * x**2 + 0.3 * x * np.sin(15 * x) + 0.05 * np.cos(50 * x)
@@ -79,6 +79,8 @@ class TestSurrogate(unittest.TestCase):
         os.rmdir(self.outdir)
 
     def test_deep_gp_model(self):
+        from compas_surrogate.surrogate.models.deep_gp_model import DeepGPModel
+
         self.gp_model_tester(DeepGPModel)
 
     def test_sklearn_gp_model(self):
@@ -86,7 +88,7 @@ class TestSurrogate(unittest.TestCase):
 
     def gp_model_tester(self, gp_model_class):
         gp_name = gp_model_class.__name__
-        pts = [10, 25, 50]
+        pts = [50]
         test_funcs = [CURVY_F]
         num_f = len(test_funcs)
 